c9ec04fbd7b68004310102fe12cb23be7548ab54
Anothays <jeremy.snnk@gmail.com>
2024-08-10 19:35:49 +0200

    add fixtures bundles and somes fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,55 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Movie;
+use App\Entity\MovieCategory;
+use DateTime;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
 class AppFixtures extends Fixture
 {
-    public function load(ObjectManager $manager): void
+
+    public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+  
+        $movie_categories = json_decode(file_get_contents(__DIR__ . '/movie_categories.json'), true);
+        $moviesData = json_decode(file_get_contents(__DIR__ . '/movies.json'), true);
+        
+        // Create MovieCategories
+        $categories = [];
+        foreach ($movie_categories as $value) {
+            $movie_category = new MovieCategory();
+            $movie_category->setCategoryName($value["categoryName"]);
+            $manager->persist(($movie_category));
+            $categories[$value["categoryName"]] = $movie_category;
+        }
+
+        foreach ($moviesData as $value) {
+            $movie = new Movie();
+            $movie
+                ->setTitle($value["title"])
+                ->setDirector($value["director"])
+                ->setDuration(new DateTime($value["duration"]))
+                ->setMinimumAge($value["minimumAge"])
+                ->setSynopsis($value["synopsis"])
+                ->setStaffFavorite($value["isStaffFavorite"])
+                ->setReleasedOn(new DateTime($value["releasedOn"]))
+                ->setNoteTotalVotes($value["noteTotalVotes"])
+                ->setNotesTotalPoints($value["notesTotalPoints"])
+                ->setPosters([])
+                ->setCasting($value["casting"]);
+             
+            foreach ($value['movieCategories'] as $categoryName) {
+                if (isset($categories[$categoryName])) {
+                    $movie->addMovieCategory($categories[$categoryName]);
+                }
+            }
+            $manager->persist($movie);
+        }
 
         $manager->flush();
+
+        
     }
 }
