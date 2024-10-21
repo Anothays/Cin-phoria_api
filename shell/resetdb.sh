54fd1e9fd5dc6e35fb66907f2c571f732e8acb3e
Anothays <jeremy.snnk@gmail.com>
2024-10-21 22:25:21 +0200

    replace symfony by php into resetdb.sh

--- shell/resetdb.sh
+++ shell/resetdb.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
 # Supprimer la base de données
-if ! symfony console d:d:d -fq; then
+if ! php bin/console d:d:d -fq; then
   echo "Erreur lors de la suppression de la base de données."
   exit 1
 fi
 
 # Créer la base de données
-if ! symfony console d:d:c -q; then
+if ! php bin/console d:d:c -q; then
   echo "Erreur lors de la création de la base de données."
   exit 1
 fi
 
 # Créer le schéma
-if ! symfony console d:s:c -q; then
+if ! php bin/console d:s:c -q; then
   echo "Erreur lors de la création du schéma de la base de données."
   exit 1
 fi
 
 # Charger les fixtures si l'option --datafixtures est passée
 if [[ "$1" == "--datafixtures" ]]; then
-  if ! symfony console d:f:l -nq; then
+  if ! php bin/console d:f:l -nq; then
     echo "Erreur lors du chargement des fixtures."
     exit 1
   fi
