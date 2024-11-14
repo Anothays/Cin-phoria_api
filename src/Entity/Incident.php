54876e2c1135acdeaf2523751e51fe74727d8945
Anothays <jeremy.snnk@gmail.com>
2024-11-14 16:41:17 +0100

    update src/Entity/Incident.php

--- src/Entity/Incident.php
+++ src/Entity/Incident.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Delete;
 use ApiPlatform\Metadata\GetCollection;
 use ApiPlatform\Metadata\Get;
 use ApiPlatform\Metadata\Patch;
@@ -18,13 +19,14 @@ use Symfony\Component\Serializer\Attribute\Groups;
 #[ApiResource(
     normalizationContext: ['groups' => ['incident']],
     denormalizationContext: ['groups' => ['incident:write']],
-    security: "is_granted('ROLE_ADMIN')",
+    security: "is_granted('ROLE_STAFF')",
     operations: [
         new GetCollection(),
         new Get(),
         new Post(),
         new Put(),
-        new Patch()
+        new Patch(),
+        new Delete()
     ]
 )]
 class Incident
@@ -32,15 +34,15 @@ class Incident
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
-    #[Groups(["incident", "incident:write"])]
+    #[Groups(["incident", "incident:write", "projectionRoom"])]
     private ?int $id = null;
 
     #[ORM\Column(type: Types::TEXT)]
-    #[Groups(["incident", "incident:write"])]
+    #[Groups(["incident", "incident:write", "projectionRoom"])]
     private ?string $description = null;
 
     #[ORM\Column]
-    #[Groups(["incident", "incident:write"])]
+    #[Groups(["incident", "incident:write", "projectionRoom"])]
     private ?\DateTimeImmutable $createdAt = null;
 
     #[ORM\Column(type: Types::DATETIME_MUTABLE)]
@@ -50,6 +52,14 @@ class Incident
     #[ORM\JoinColumn(nullable: false)]
     #[Groups(["incident", "incident:write"])]
     private ?ProjectionRoom $projectionRoom = null;
+
+    #[ORM\Column]
+    private ?bool $isResolved = false;
+
+    public function __construct() {
+        $this->createdAt = new \DateTimeImmutable();
+        $this->updatedAt = new \DateTime();
+    }
 
     public function getId(): ?int
     {
@@ -100,6 +110,18 @@ class Incident
     public function setProjectionRoom(?ProjectionRoom $projectionRoom): static
     {
         $this->projectionRoom = $projectionRoom;
+
+        return $this;
+    }
+
+    public function isResolved(): ?bool
+    {
+        return $this->isResolved;
+    }
+
+    public function setResolved(bool $isResolved): static
+    {
+        $this->isResolved = $isResolved;
 
         return $this;
     }
