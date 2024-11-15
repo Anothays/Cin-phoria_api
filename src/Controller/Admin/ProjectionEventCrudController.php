a8ace199503f4e0dd95eeae88929478dff39169f
Anothays <jeremy.snnk@gmail.com>
2024-11-15 14:56:19 +0100

    add detail action on ProjectionEventCrudController

--- src/Controller/Admin/ProjectionEventCrudController.php
+++ src/Controller/Admin/ProjectionEventCrudController.php
@@ -2,22 +2,17 @@
 
 namespace App\Controller\Admin;
 
-use App\Entity\Movie;
 use App\Entity\ProjectionEvent;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
-use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\FormField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class ProjectionEventCrudController extends AbstractCrudController
@@ -56,6 +51,7 @@ class ProjectionEventCrudController extends AbstractCrudController
     {
         return parent::configureActions($actions)
         ->update(Crud::PAGE_INDEX, Action::NEW, fn ($action) => $action->setLabel('Nouvelle séance'))
+        ->add(Crud::PAGE_INDEX, Action::DETAIL);
         ;
     }
 }
