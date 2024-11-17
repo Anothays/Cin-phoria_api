bb8bf1c372ff2eadd058e054d12c8c6eed0a768a
Anothays <jeremy.snnk@gmail.com>
2024-11-17 20:00:12 +0100

    Admin can create Employee from back-office

--- src/Controller/Admin/UserStaffCrudController.php
+++ src/Controller/Admin/UserStaffCrudController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Admin;
 
 use App\Entity\UserStaff;
+use Doctrine\ORM\EntityManagerInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
@@ -11,9 +12,15 @@ use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserStaffCrudController extends AbstractCrudController
 {
+
+    public function __construct(private UserPasswordHasherInterface $userPasswordHasher){}
+
     public static function getEntityFqcn(): string
     {
         return UserStaff::class;
@@ -27,8 +34,16 @@ class UserStaffCrudController extends AbstractCrudController
             TextField::new('lastname', 'Nom')->onlyOnForms(),
             TextField::new('firstname', 'Prénom')->onlyOnForms(),
             EmailField::new('email'),
-            ArrayField::new('roles', 'Rôles'),
-            DateTimeField::new('createdAt', 'inscrit le'),
+            TextField::new('password', 'Mot de passe')
+            ->onlyWhenCreating()
+            ->setFormType(RepeatedType::class)
+            ->setFormTypeOptions([
+                'type' => PasswordType::class,
+                'first_options' => ['label' => 'Mot de passe'],
+                'second_options' => ['label' => 'Confirmez le mot de passe'],
+            ]),
+            ArrayField::new('roles', 'Rôles')->hideOnForm(),
+            DateTimeField::new('createdAt', 'inscrit le')->hideOnForm(),
         ];
     }
 
@@ -48,7 +63,18 @@ class UserStaffCrudController extends AbstractCrudController
     {
         return parent::configureActions($actions)
         ->setPermission(Action::DELETE, 'ROLE_ADMIN')
+        ->setPermission(Action::NEW, 'ROLE_ADMIN')
         ->setPermission(Action::EDIT, 'ROLE_ADMIN')
         ;
+    }
+
+    public function persistEntity(EntityManagerInterface $entityManager, $entityInstance): void
+    {
+        $entityInstance->setRoles(["ROLE_STAFF"]);
+
+        /** @var UserStaff $entityInstance */
+        $entityInstance->setPassword($this->userPasswordHasher->hashPassword($entityInstance, $entityInstance->getPassword()));
+        parent::persistEntity($entityManager, $entityInstance);
+
     }
 }
