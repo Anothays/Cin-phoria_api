ed09cdf3493202668d48131f3ce6db0ad1df6e32
Anothays <jeremy.snnk@gmail.com>
2024-11-18 12:16:42 +0100

    handle user creation from back-office

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -3,18 +3,26 @@
 namespace App\Controller\Admin;
 
 use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserCrudController extends AbstractCrudController
 {
+
+    public function __construct(private UserPasswordHasherInterface $userPasswordHasher){}
+
     public static function getEntityFqcn(): string
     {
         return User::class;
@@ -25,9 +33,23 @@ class UserCrudController extends AbstractCrudController
     {
         return [
             TextField::new('fullName', 'Nom')->hideOnForm(),
-            TextField::new('lastname', 'Nom')->onlyOnForms(),
             TextField::new('firstname', 'Prénom')->onlyOnForms(),
+            TextField::new('lastname', 'Nom')->onlyOnForms(),
+            TextField::new('password', 'Mot de passe')
+            ->onlyWhenCreating()
+            ->setFormType(RepeatedType::class)
+            ->setFormTypeOptions([
+                'type' => PasswordType::class,
+                'first_options' => ['label' => 'Mot de passe'],
+                'second_options' => ['label' => 'Confirmez le mot de passe'],
+            ]),
             EmailField::new('email'),
+            BooleanField::new('verified', 'email vérifié')
+            ->hideOnForm()
+            ->renderAsSwitch(false),
+            BooleanField::new('verified', 'email vérifié')
+            ->onlyOnForms()
+            ->setFormTypeOption('data', true),
             ArrayField::new('roles', 'Rôles'),
             DateTimeField::new('createdAt', 'inscrit le'),
             AssociationField::new('reservations', 'Réservations')->hideOnForm()
@@ -52,5 +74,15 @@ class UserCrudController extends AbstractCrudController
         ->setPermission(Action::DELETE, 'ROLE_ADMIN')
         ->setPermission(Action::EDIT, 'ROLE_ADMIN')
         ;
+    }
+
+    public function persistEntity(EntityManagerInterface $entityManager, $entityInstance): void
+    {
+        $entityInstance->setRoles(["ROLE_USER"]);
+
+        /** @var User $entityInstance */
+        $entityInstance->setPassword($this->userPasswordHasher->hashPassword($entityInstance, $entityInstance->getPassword()));
+        parent::persistEntity($entityManager, $entityInstance);
+
     }
 }
